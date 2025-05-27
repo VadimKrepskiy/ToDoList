3abc4cc782968989aafc1be8d90006bf1c0e0db9
VadimKrepskiy <krepskiy.vadim@mail.ru>
2025-05-27 15:19:28 +0300

    TDL-20 fix: add base-name for browser-router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {App} from '@/app/App.tsx'
 import {BrowserRouter} from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <BrowserRouter basename='/todo-list'>
         <Provider store={store}>
             <App />
         </Provider>
